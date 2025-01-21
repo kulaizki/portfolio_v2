025f20bb4523efdf7f27a5b241d80fddff0fb715
kulaizki <kulaizke@gmail.com>
2025-01-22 00:10:30 +0800

    refactor: correct favicon file path

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,7 +8,7 @@
 	<meta name="description" content="Explore Fitz's developer portfolio featuring expertise in software engineering, mobile app development, and UI/UX design." />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<meta charset="UTF-8" />
-	<link rel="icon" href="/favicon.png" />
+	<link rel="icon" href="/images/favicon.png" />
 </svelte:head>
 
 <main>
