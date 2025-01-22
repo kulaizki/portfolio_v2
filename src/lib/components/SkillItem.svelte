9e453d25f90b056e55c67e1ccde6270eda1ce443
kulaizki <kulaizke@gmail.com>
2025-01-22 15:23:41 +0800

    style: add hover glow to SkillItem

--- src/lib/components/SkillItem.svelte
+++ src/lib/components/SkillItem.svelte
@@ -7,7 +7,7 @@
   href={skillUrl}
   target="_blank"
   rel="noopener noreferrer"
-  class="px-4 py-2 bg-gray-600 rounded-lg shadow-md hover:bg-sky-500 text-white transition-all duration-300"
+  class="px-4 py-2 bg-gray-600 rounded-lg shadow-md hover:bg-sky-500 text-white transition-all ease-in-out duration-300 hover:shadow-[0_0_15px_rgba(56,189,248,0.7)]"
 >
   {skillName}
 </a>
